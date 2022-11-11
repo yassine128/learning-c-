fd897734e6ba3ef8917a0f8189e15b2d55345091
Seddaoui Yassine <yassineseddaoui@gmail.com>
2022-11-11 15:20:17 -0500

    Fibonacci with c++

--- main.cpp
+++ main.cpp
@@ -4,20 +4,19 @@
 
 using namespace std;
 
+int fib(int n) {
+    if ( (n==1) || (n==0) ) {
+        return n;
+    }
+    else {
+        return (fib(n-1) + fib(n-2));
+    }
+}
+
 int main()
 {
-    // Print 
-    cout << "Print something" << endl;
-
-    //Variable
-    int  n = 5; 
-    string m = "mot";
-    char c = 'm';
-    float f = 0.5;
-
-    //for loop
-    for (int i = 0; i < 5; i++) {
-        cout << i << endl;
-    }
-
+  int myVar;
+  cout << "Value n: ";
+  cin >> myVar;
+  cout << fib(myVar);
 }
