17cbd8a2e70f78d9c7385f8a16e4b9833e17d498
Seddaoui Yassine <yassineseddaoui@gmail.com>
2022-11-11 10:18:37 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Print 
+    cout << "Print something" << endl;
+
+    //Variable
+    int  n = 5; 
+    string m = "mot";
+    char c = 'm';
+    float f = 0.5;
+
+    //for loop
+    for (int i = 0; i < 5; i++) {
+        cout << i << endl;
+    }
+
+}
